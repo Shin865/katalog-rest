5520b827a4f54d0c4e32fd33608062b5bb59e022
Shin865 <129364414+Shin865@users.noreply.github.com>
2024-04-09 22:11:18 +0700

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,44 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import data from '../public/data/DATA.json';
+
+const getRestaurant = (data) => {
+  data.restaurants.forEach(restaurant => {
+      const restaurantItem = document.querySelector('.posts');
+      restaurantItem.innerHTML += `
+        <article class="post-item">
+          <img
+            class="post-item_thumbnail"
+            src="${restaurant.pictureId}" 
+            alt="${restaurant.name}"
+          />
+          <div class="post-item_content">
+            <p class="post-item_rating">Rating: ${restaurant.rating}</p>
+            <h1 class="post-item_title">${restaurant.name}</h1>
+            <p class="post-item_description">${restaurant.description}</p>
+            <p class="post-item_city">Location: ${restaurant.city}</p>
+          </div>
+        </article>
+      `;
+  })
+}
+getRestaurant(data);
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+  drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+  drawer.classList.remove('open');
+});
